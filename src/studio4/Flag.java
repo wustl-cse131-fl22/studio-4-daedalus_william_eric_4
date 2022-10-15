fbdbb14c8232cd21d2684b57ae1635cf88553d74
willi <willi@Will_C_Dell.dhcp.wustl.edu>
2022-10-15 11:16:21 -0500

    Studio 4 (10/25/2022)
    Flag
    Interpret Drawing File

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,66 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
+	public static void drawFlag(double[] position) {
+		StdDraw.clear();
+		StdDraw.setPenColor((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
+		StdDraw.filledCircle(position[0], position[1], 0.3);
+		StdDraw.setPenColor((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
+		StdDraw.filledCircle(1-position[0], position[1], 0.3);
+		StdDraw.setPenColor((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
+		StdDraw.filledCircle(1-position[0], 1-position[1], 0.3);
+		StdDraw.setPenColor((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
+		StdDraw.filledCircle(position[0], 1- position[1], 0.3);
+		StdDraw.show();
+	}
+	
+	public static void updateFlag(boolean[] directions, double[] position, double distance) {
+		if(directions[0]) {
+			position[1] += distance;
+		}
+		else {
+			position[1] -= distance;
+		}
+		if(directions[2]) {
+			position[0] -= distance;
+		}
+		else {
+			position[0] += distance;
+		}
+		if(position[1] >= 0.7) {
+			directions[1] = true;
+			directions[0] = false;
+		}
+		if(position[1] < 0.3) {
+			directions[0] = true;
+			directions[1] = false;
+		}
+		if(position[0] >= 0.7) {
+			directions[2] = true;
+			directions[3] = false;
+		}
+		if(position[0] < 0.3) {
+			directions[3] = true;
+			directions[2] = false;
+		}
+	}
+	
 	public static void main(String[] args) {
-		
+		boolean[] dir = new boolean[4];
+		double[] pos = new double[2];
+		pos[0] = 0.3;
+		pos[1] = 0.5;
+		int up = 0;
+		int down = 1;
+		int left = 2;
+		int right = 3;
+		dir[up] = true;
+		dir[down] = false;
+		dir[left] = true;
+		dir[right] = false;
+		while(true) {
+			drawFlag(pos);
+			updateFlag(dir, pos, 0.0005);
+		}
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,37 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		if(shape.equals("ellipse")) {
+			StdDraw.setPenColor(red, green, blue);
+			if(in.nextBoolean()) {
+				StdDraw.filledEllipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else StdDraw.ellipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+		}
+		if(shape.equals("rectangle")) {
+			StdDraw.setPenColor(red, green, blue);
+			if(in.nextBoolean()) {
+				StdDraw.filledRectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else StdDraw.rectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+		}
+		if(shape.equals("triangle")) {
+			StdDraw.setPenColor(red, green, blue);
+			boolean bool = in.nextBoolean();
+			double[] xs = new double[3];
+			double[] ys = new double[3];
+			for(int i = 0; i < 3; i++) {
+				xs[i] = in.nextDouble();
+				ys[i] = in.nextDouble();
+			}
+			if(bool) {
+				StdDraw.filledPolygon(xs,ys);
+			}
+			else StdDraw.polygon(xs,ys);
+		}
 	}
 }
